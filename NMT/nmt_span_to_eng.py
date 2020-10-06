df785fa1606d53bdd5c3017e5dcd7c0de8bd4e5e
Anirudh Rajiv Menon <36445587+axe76@users.noreply.github.com>
2020-10-06 12:12:35 +0530

    Add files via upload

---
+++ NMT/nmt_span_to_eng.py
@@ -0,0 +1,262 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  3 00:03:36 2020
+
+@author: ACER
+"""
+import tensorflow as tf
+
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
+import unicodedata
+import re
+import numpy as np
+import os
+import time
+
+#path_to_zip = tf.keras.utils.get_file('spa-eng.zip', origin='http://download.tensorflow.org/data/spa-eng.zip', extract=True)
+
+#path_to_file = os.path.dirname(path_to_zip)+"/spa-eng/spa.txt"
+
+path_to_file = "spa-eng/spa.txt"
+
+def unicode_to_ascii(s):
+    return ''.join(c for c in unicodedata.normalize('NFD', s) if unicodedata.category(c) != 'Mn')
+
+def preprocess_sentence(w):
+    w = unicode_to_ascii(w.lower().strip())
+    w = re.sub(r"([?.!,¿])", r" \1 ", w)
+    w = re.sub(r'[" "]+', " ", w)
+    
+    # replacing everything with space except (a-z, A-Z, ".", "?", "!", ",")
+    w = re.sub(r"[^a-zA-Z?.!,¿]+", " ", w)
+    
+    w = w.rstrip().strip()
+    w = '<start> ' + w + ' <end>'
+    return w
+
+def create_dataset(path, num_examples):
+    lines = open(path, encoding='UTF-8').read().strip().split('\n')
+    
+    word_pairs = [[preprocess_sentence(w) for w in l.split('\t')]  for l in lines[:num_examples]]
+    
+    return word_pairs
+
+class LanguageIndex():
+  def __init__(self, lang):
+    self.lang = lang
+    self.word2idx = {}
+    self.idx2word = {}
+    self.vocab = set()
+    self.create_index()
+    
+  def create_index(self):
+    for phrase in self.lang:
+      self.vocab.update(phrase.split(' '))
+    
+    self.vocab = sorted(self.vocab)
+    
+    self.word2idx['<pad>'] = 0
+    for index, word in enumerate(self.vocab):
+      self.word2idx[word] = index + 1
+    
+    for word, index in self.word2idx.items():
+      self.idx2word[index] = word
+      
+def max_length(tensor):
+    return max(len(t) for t in tensor)      
+
+def load_dataset(path, num_examples):
+    pairs = create_dataset(path, num_examples) 
+    inp_lang = LanguageIndex(sp for en, sp in pairs)
+    targ_lang = LanguageIndex(en for en, sp in pairs)
+    
+    # Spanish sentences
+    input_tensor = [[inp_lang.word2idx[s] for s in sp.split(' ')] for en, sp in pairs]
+    
+    # English sentences
+    target_tensor = [[targ_lang.word2idx[s] for s in en.split(' ')] for en, sp in pairs]
+    
+    max_length_inp, max_length_tar = max_length(input_tensor), max_length(target_tensor)
+    
+    input_tensor = tf.keras.preprocessing.sequence.pad_sequences(input_tensor, 
+                                                                 maxlen=max_length_inp,
+                                                                 padding='post')
+    
+    target_tensor = tf.keras.preprocessing.sequence.pad_sequences(target_tensor, 
+                                                                  maxlen=max_length_tar, 
+                                                                  padding='post')
+    
+    return input_tensor, target_tensor, inp_lang, targ_lang, max_length_inp, max_length_tar
+
+
+num_examples = 30000
+input_tensor, target_tensor, inp_lang, targ_lang, max_length_inp, max_length_targ = load_dataset(path_to_file, num_examples)
+
+input_tensor_train, input_tensor_val, target_tensor_train, target_tensor_val = train_test_split(input_tensor, target_tensor, test_size=0.2)
+
+
+BUFFER_SIZE = len(input_tensor_train)
+BATCH_SIZE = 64
+embedding_dim = 256
+units = 1024
+vocab_inp_size = len(inp_lang.word2idx)
+vocab_tar_size = len(targ_lang.word2idx)
+
+dataset = tf.data.Dataset.from_tensor_slices((input_tensor_train, target_tensor_train)).shuffle(BUFFER_SIZE)
+dataset = dataset.batch(BATCH_SIZE,drop_remainder=True)
+
+def gru(units):
+    return tf.keras.layers.GRU(units, return_sequences=True, return_state=True, recurrent_activation='sigmoid', recurrent_initializer='glorot_uniform')
+
+class Encoder(tf.keras.Model):
+    def __init__(self, vocab_size, embedding_dim, enc_units, batch_sz):
+        super(Encoder, self).__init__()
+        self.batch_sz = batch_sz
+        self.enc_units = enc_units
+        self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
+        self.gru = gru(self.enc_units)
+        
+    def call(self, x, hidden):
+        x = self.embedding(x)
+        output, state = self.gru(x, initial_state = hidden)        
+        return output, state
+    
+    def initialize_hidden_state(self):
+        return tf.zeros((self.batch_sz, self.enc_units))
+    
+class Decoder(tf.keras.Model):
+    def __init__(self, vocab_size, embedding_dim, dec_units, batch_sz):
+        super(Decoder, self).__init__()
+        self.batch_sz = batch_sz
+        self.dec_units = dec_units
+        self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
+        self.gru = gru(self.dec_units)
+        self.fc = tf.keras.layers.Dense(vocab_size)
+        
+        # used for attention
+        self.W1 = tf.keras.layers.Dense(self.dec_units)
+        self.W2 = tf.keras.layers.Dense(self.dec_units)
+        self.V = tf.keras.layers.Dense(1)
+        
+    def call(self, x, hidden, enc_output):
+        hidden_with_time_axis = tf.expand_dims(hidden, 1)
+        score = tf.nn.tanh(self.W1(enc_output) + self.W2(hidden_with_time_axis))
+        
+        attention_weights = tf.nn.softmax(self.V(score), axis=1)
+        
+        context_vector = attention_weights * enc_output
+        context_vector = tf.reduce_sum(context_vector, axis=1)
+
+        x = self.embedding(x)
+
+        x = tf.concat([tf.expand_dims(context_vector, 1), x], axis=-1)
+        
+        # passing the concatenated vector to the GRU
+        output, state = self.gru(x)
+        
+        output = tf.reshape(output, (-1, output.shape[2]))
+        x = self.fc(output)
+        
+        return x, state, attention_weights
+        
+    def initialize_hidden_state(self):
+        return tf.zeros((self.batch_sz, self.dec_units))
+    
+encoder = Encoder(vocab_inp_size, embedding_dim, units, BATCH_SIZE)
+decoder = Decoder(vocab_tar_size, embedding_dim, units, BATCH_SIZE)
+
+optimizer = tf.keras.optimizers.Adam()
+
+def loss_function(real, pred):
+  mask = 1 - np.equal(real, 0)
+  loss_ = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=real, logits=pred) * mask
+  return tf.reduce_mean(loss_)
+
+EPOCHS = 10
+for epoch in range(EPOCHS):
+    start = time.time()
+    
+    hidden = encoder.initialize_hidden_state()
+    total_loss = 0
+    
+    for (batch, (inp, targ)) in enumerate(dataset):
+        loss = 0
+        
+        with tf.GradientTape() as tape:
+            enc_output, enc_hidden = encoder(inp, hidden)
+            
+            dec_hidden = enc_hidden
+            
+            dec_input = tf.expand_dims([targ_lang.word2idx['<start>']] * BATCH_SIZE, 1)       
+
+            for t in range(1, targ.shape[1]):
+                predictions, dec_hidden, _ = decoder(dec_input, dec_hidden, enc_output)
+                
+                loss += loss_function(targ[:, t], predictions)
+                
+                # using teacher forcing
+                dec_input = tf.expand_dims(targ[:, t], 1)
+        
+        total_loss += (loss / int(targ.shape[1]))
+        
+        variables = encoder.variables + decoder.variables
+        
+        gradients = tape.gradient(loss, variables)
+      
+        optimizer.apply_gradients(zip(gradients, variables), tf.compat.v1.train.get_or_create_global_step())
+
+        if batch % 100 == 0:
+            print('Epoch {} Batch {} Loss {:.4f}'.format(epoch + 1,
+                                                         batch,
+                                                         loss.numpy() / int(targ.shape[1])))
+    
+    print('Epoch {} Loss {:.4f}'.format(epoch + 1,
+                                        total_loss/len(input_tensor)))
+    print('Time taken for 1 epoch {} sec\n'.format(time.time() - start))
+  
+    
+def evaluate(sentence, encoder, decoder, inp_lang, targ_lang, max_length_inp, max_length_targ):
+    #attention_plot = np.zeros((max_length_targ, max_length_inp))
+    
+    sentence = preprocess_sentence(sentence)
+
+    inputs = [inp_lang.word2idx[i] for i in sentence.split(' ')]
+    inputs = tf.keras.preprocessing.sequence.pad_sequences([inputs], maxlen=max_length_inp, padding='post')
+    inputs = tf.convert_to_tensor(inputs)
+    
+    result = ''
+
+    hidden = [tf.zeros((1, units))]
+    enc_out, enc_hidden = encoder(inputs, hidden)
+
+    dec_hidden = enc_hidden
+    dec_input = tf.expand_dims([targ_lang.word2idx['<start>']], 0)
+
+    for t in range(max_length_targ):
+        predictions, dec_hidden, attention_weights = decoder(dec_input, dec_hidden, enc_out)
+        
+        predicted_id = tf.compat.v1.multinomial(tf.exp(predictions), num_samples=1)[0][0].numpy()
+
+        result += targ_lang.idx2word[predicted_id] + ' '
+
+        if targ_lang.idx2word[predicted_id] == '<end>':
+            return result, sentence
+        
+        # the predicted ID is fed back into the model
+        dec_input = tf.expand_dims([predicted_id], 0)
+
+    return result, sentence
+
+def translate(sentence, encoder, decoder, inp_lang, targ_lang, max_length_inp, max_length_targ):
+    result, sentence = evaluate(sentence, encoder, decoder, inp_lang, targ_lang, max_length_inp, max_length_targ)
+    #, attention_plot    
+    print('Input: {}'.format(sentence))
+    print('Predicted translation: {}'.format(result))
+
+    
+translate('hace mucho frio aqui.', encoder, decoder, inp_lang, targ_lang, max_length_inp, max_length_targ)
+translate('esta es mi vida.', encoder, decoder, inp_lang, targ_lang, max_length_inp, max_length_targ)
+translate('¿todavia estan en casa?', encoder, decoder, inp_lang, targ_lang, max_length_inp, max_length_targ)
+translate('trata de averiguarlo.', encoder, decoder, inp_lang, targ_lang, max_length_inp, max_length_targ)
